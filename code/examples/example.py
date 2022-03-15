25b17b8c1f8a6cc1194cf1836bce49c29d32519d
PackTheCommand <92752763+PackTheCommand@users.noreply.github.com>
2022-03-15 21:54:49 +0100

    Update example.py

--- code/examples/example.py
+++ code/examples/example.py
@@ -1,27 +1,15 @@
 from Vsvpack import *
-#from Vsvpack import absoluteScriptFolderPath
-
-#--definitions
-filrabsolute=absoluteScriptFolderPath()
-saveFile=vsvpack()
-
 #get Script path
-usableFilePath=filrabsolute.get(__file__)
+path1=absoluteScriptFolderPath().get(__file__)
 
-#difine File(-setings)
-file_name="example_file"
-path2=usableFilePath+"\\"+file_name
-example_variable="Variable_Name2"
-
-
-#creating file
-saveFile.new(usableFilePath,file_name,True)
-#writing Variable to file
-saveFile.add(path2,example_variable,"x:"+"y:"+"z")
-#reading from file
-test=saveFile.read(path2,example_variable)
-#removing Variable from file
-saveFile.remove(path2,example_variable)
-#result:
-print(test)
+saveFile=vsvpack(path1,"I_am_a_SaveFile")
+#create File
+saveFile.new(False)
+#Add save value
+saveFile.add("Test","TestValue")
+#read Variable Value
+print(saveFile.read("Test"))
+#remove saveValue
+saveFile.remove("Test")
+# to destroy (make unusable) you can use(but you shouldn't): saveFile.aVoidValue()
 
